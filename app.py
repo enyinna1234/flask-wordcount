6e2e25663f09934ec661a967148b00200f583a15
Enyinna Ochulor <enyi@live.com>
2018-11-05 13:50:22 -0800

    fixing typo in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_objects(os.environment['APP_SETTINGS'])
+app.config.from_object(os.environment['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
